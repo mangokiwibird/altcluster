a2b611a177536a7e0763cf9df9d8febff0ffda15
dolphin2410 <dolphin2410@outlook.kr>
2025-05-28 10:07:07 +0900

    auto pin on download

--- src/ipfs.ts
+++ src/ipfs.ts
@@ -1,12 +1,16 @@
-import { create } from 'kubo-rpc-client'
+import { CID, create } from 'kubo-rpc-client'
+import fs from 'fs'
+import { v4 as uuidv4 } from 'uuid'
 
-export async function fetch_file_ipfs(CID: string): Promise<Buffer> {
+export async function fetch_file_ipfs(cidhash: string): Promise<Buffer> {
     const client = create()
     let total = new Uint8Array()
 
-    for await (const buf of client.get(CID)) {
+    for await (const buf of client.get(CID.parse(cidhash))) {
         total = Buffer.concat([total, buf])
     }
+
+    client.pin.add(CID.parse(cidhash))
 
     return Buffer.from(total.buffer)
 }
@@ -15,4 +19,10 @@ export async function upload_file_ipfs(contents: string): Promise<string> {
     const client = create()
     const { cid } = await client.add(contents)
     return cid.toString()
+}
+
+export async function download_file_ipfs(CID: string): Promise<string> {
+    const buffer = await fetch_file_ipfs(CID)
+    fs.writeFileSync(`altcluster_pins/${CID}`, buffer)
+    return `altcluster_pins/${CID}`
 }
